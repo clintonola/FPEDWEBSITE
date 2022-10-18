50cdc8ff97de940efcb28535590cdcd437a26572
clintonola <63821901+clintonola@users.noreply.github.com>
2022-10-18 11:42:38 -0400

    making changes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
-import express, { Response } from 'express'
+import express, { Request, Response } from 'express'
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (res: Response) => {
-  res.send('hello')
+app.set('view engine', 'ejs')
+
+app.get('/', (_req: Request, res: Response) => {
+  res.render('index')
+})
+
+app.get('/users/register', (_req: Request, res: Response) => {
+  res.render('register')
+})
+
+app.get('/users/login', (_req: Request, res: Response) => {
+  res.render('login')
+})
+
+app.get('/users/display', (_req: Request, res: Response) => {
+  res.render('display', { user: 'clinton' })
 })
 
 app.listen(PORT, () => console.log(`Sever running on port ${PORT}`))
